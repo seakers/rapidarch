838c678b8a1888f72673e6aa6f67384cde8ece43
bgy36ww <david36ww@hotmail.com>
2016-09-09 17:25:52 -0700

    update pugh_matrix_and_ahp.py
    Added rand_comp_mat method

--- Python/pugh matrix and ahp/pugh_matrix_and_ahp.py
+++ Python/pugh matrix and ahp/pugh_matrix_and_ahp.py
@@ -17,3 +17,19 @@ def consistency_index(A):
     RI=[0,0,0.58,0.9,1.12,1.24,1.32,1.41,1.45,1.49]
     CR=CI/RI[n]
     return CR
+
+def rand_comp_mat(n):
+    # cm = rand_comp_mat(n)
+    # this function generates a random reciprocal comparison matrix of size n
+    # inputs: n (size)
+    # outputs: cm, an n x n reciprocal comparison matrix
+    # Daniel Selva <ds925@cornell.edu>
+    # 9/3/15
+    #
+    cm=np.random.random_integers(1,9,(n,n))
+    for i in range(0,n):
+        cm[i][i]=1
+    for i in range(1,n):
+        for j in range(0,i):
+            cm[i][j]=1/(cm[i][j])
+    return cm
